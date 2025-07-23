12de9ff62a50672a36cfbbf84a2c4004d37fb8ab
KISHAN YADAV <108346805+KISHAN-Y@users.noreply.github.com>
2025-07-23 22:43:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -107,7 +107,7 @@ const setupLogoutButtons = () => {
     if (btn.innerHTML.includes('fa-sign-out-alt')) {
       btn.addEventListener('click', () => {
         destroySession();
-        window.location.href = 'Leaf-Lore/index.html';
+        window.location.href = 'index.html';
       });
     }
   });
